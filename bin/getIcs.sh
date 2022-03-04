0673232902e2a32f2da8076018a1f7d2931dc82b
bernhara <raphael.bernhard@orange.fr>
2022-03-04 10:36:21 +0100

    pb with cygwin

--- bin/getIcs.sh
+++ bin/getIcs.sh
@@ -1,13 +1,51 @@
 #! /bin/bash
 
 HERE=$( dirname "$0" )
+PROJECT_ROOT_DIR="${HERE}/.."
 
-source "../${HERE}/dav_config.env"
+source "${PROJECT_ROOT_DIR}/dav_config.env"
 
-ics_url='https://cloud.forumtestplanetecitroen.fr/remote.php/dav/calendars/servicebox/personal/sabredav-08153cd3-9112-46fb-b7c8-cceb7c58564e.ics'
+: ${PYTHON_BIN:="${PROJECT_ROOT_DIR}/.venv/Scripts/python"}
 
-vcal=$(
-    curl -u ${CALDAV_USERNAME}:${CALDAV_PASSWORD} -H 'Accept: text/calendar' -H 'Accept-Charset: utf-8, iso-8859-1;q=0.5' "${ics_url}" | \
-	sed -z 's/\r\n\ //g'
-    )
+getVCalData ()
+{
+    ics_url="$1"
+
+    sanitized_url=$(
+	echo "${ics_url}" | tr --delete '\r'
+	)
+
+    vcal_data=$(
+	curl -u ${CALDAV_USERNAME}:${CALDAV_PASSWORD} -H 'Accept: text/calendar' -H 'Accept-Charset: utf-8, iso-8859-1;q=0.5' "${ics_url}" | \
+	    sed -z 's/\r\n\ //g'
+	     )
+
+    echo "${vcal_data}"
+}
+
+ics_url_list=$( ${PYTHON_BIN} "${PROJECT_ROOT_DIR}/src/getAppointments4Date.py" 2>/dev/null )
+XX=$( ${PYTHON_BIN} "${PROJECT_ROOT_DIR}/src/getAppointments4Date.py" 2>/dev/null )
+
+echo "${ics_url_list}"
+echo ${ics_url_list}
+exit 1
+
+${PYTHON_BIN} "${PROJECT_ROOT_DIR}/src/getAppointments4Date.py" >/tmp/zz.txt
+
+${PYTHON_BIN} "${PROJECT_ROOT_DIR}/src/getAppointments4Date.py" 2 >& 9
+
+echo "${ics_url_list}"
+cat /tmp/zz.txt
+cat <& 9
+exit 1
+
+for ics_url in $( echo "${ics_url_list}" )
+do
+
+    vcal_data=$( getVCalData ${ics_url} )
+
+    echo ${vcal_data}
+    echo
+
+done
 

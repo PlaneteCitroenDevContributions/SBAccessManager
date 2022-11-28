73aa1efca8ae5c18ae1ddd312ba51f485d528cd6
bernhara <raphael.bernhard@orange.fr>
2022-11-28 17:01:22 +0100

    Revert "start to convert list of DNs to array toprevent space pbs"
    This reverts commit e23c44db596112b19afcfe1b9e2dabcd513b5206.

--- bin/updateGroupsFromAppointements.sh
+++ bin/updateGroupsFromAppointements.sh
@@ -62,8 +62,7 @@ ics_url_list=$(
 # search for all users who reserved
 #
 
-declare -a allowedLdapDNs=()
-
+allowedLdapDNs=''
 for ics_url in $( echo "${ics_url_list}" )
 do
 
@@ -114,18 +113,17 @@ Strings \"${lowercase_mailto}\" and \"${lowercase_ldap_mail}\" dos not match" 1>
     
     ldap_dn=$( sed -n -e '/^dn: /s/^dn: //p' <<< ${dn_search_result} )
 
-    allowedLdapDNs+=("${ldap_dn}")
+    allowedLdapDNs="${allowedLdapDNs} ${ldap_dn}"
 
 done
 
 # FIXME: rename this var
-# TODO: must be converted to an array
 already_allowed_dns=$( ${dsidm_cmd} group members "${ALLOWING_LDAP_GROUP_NAME}" | sed -e '/^dn: /s/^dn: //' )
 
 #
 # allow new users who reserved and are not already allowed
 #
-for dn in "${allowedLdapDNs[@]}"
+for dn in ${allowedLdapDNs}
 do
     if grep -q "${dn}" <<< ${already_allowed_dns}
     then
@@ -142,7 +140,7 @@ done
 
 for dn in ${already_allowed_dns}
 do
-    if grep -q "${dn}" <<< "${allowedLdapDNs[@]}"
+    if grep -q "${dn}" <<< ${allowedLdapDNs}
     then
 	:
     else

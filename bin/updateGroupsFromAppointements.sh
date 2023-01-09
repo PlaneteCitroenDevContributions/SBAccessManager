616fbed941bb103ff189927d55f12319d09e4eca
bernhara <raphael.bernhard@orange.fr>
2023-01-09 15:01:15 +0100

    add a comment which explains what to do

--- bin/updateGroupsFromAppointements.sh
+++ bin/updateGroupsFromAppointements.sh
@@ -131,6 +131,12 @@ done
     done
 ) 1>&2
 
+#
+# TODO:
+# check if a predefined DN (a manager?) has appointment currently reserver
+# if not, no new users can be allowed => allowed_DNs_array should remain empty
+#
+
 allowed_DNs_ldap_search_result=$(
 
     # dsidm may return line with empty DNs => remove these empty lines

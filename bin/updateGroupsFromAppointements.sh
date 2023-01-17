66de45ad51d9f8ecd817c446d3da9b730f6f1dc8
bernhara <raphael.bernhard@orange.fr>
2023-01-17 08:17:18 +0100

    handle the case of appointment wich is the unlocking one

--- bin/updateGroupsFromAppointements.sh
+++ bin/updateGroupsFromAppointements.sh
@@ -66,6 +66,10 @@ fi
 #
 # search in appointments if there is one which enables reservation
 #
+
+_unlocking_appoitment_found=false
+_unlocking_appointment_icl_url=''
+
 for ics_url in $( echo "${ics_url_list}" )
 do
 
@@ -80,10 +84,9 @@ do
 	    echo "INFO: found appointment without ORGANIZER => it is myself (\"${CALDAV_USERNAME}\")"
 	) 1>&2
 
-
-	#
-	# TODO: this case enables reservation
-	# NYI
+	_unlocking_appoitment_found=true
+	_unlocking_appointment_icl_url="${ics_url}"
+	break
     fi
 
 done
@@ -94,59 +97,70 @@ done
 
 declare -a appointed_DNs_array=()
 
-for ics_url in $( echo "${ics_url_list}" )
-do
-
-    vcal_data=$( getVCalData ${ics_url} )
-
-    organizer_line=$(
-	echo "${vcal_data}" | grep -e '^ORGANIZER;'
-		  )
-
-    organizer_data=$(
-	echo "${organizer_line}" | sed -e 's/ORGANIZER;//' -e 's/\r$//'
-	     )
-
-    displayName=$( echo "${organizer_data}" | sed -e 's/CN=\(.*\):mailto:.*$/\1/' )
-    mailto=$( echo "${organizer_data}" | sed -e 's/.*:mailto:\(.*\)$/\1/' )
-
-    (
-	echo "INFO: found appointment for display Name \"${displayName}\" with email \"${mailto}\""
-    ) 1>&2
-
-    # retrieve user description (dn + mail) in LDAP, based on his email address (mailto)
-    dn_search_result=$(
-	${ldapsearch_cmd} -z 1 "mail=${mailto}" dn mail
-    )
-    if grep -q '--regexp=^mail:' <<< ${dn_search_result}
-    then
-	# ldap search result OK
-	:
-    else
-	echo "INTERNAL ERROR: Could not file \"${mailto}\" in ldap" 1>&2
-	continue
-	# NOT REACHED
-    fi
-
-    #
-    # for security, we check that the retrieved mail is what we searched for
-    #
-    ldap_mail=$( sed -n -e '/^mail: /s/^mail: //p' <<< ${dn_search_result} )
-    lowercase_mailto=$( tr '[:upper:]' '[:lower:]' <<< ${mailto} )
-    lowercase_ldap_mail=$( tr '[:upper:]' '[:lower:]' <<< ${ldap_mail} )
-    if [[ "${lowercase_mailto}" != "${lowercase_ldap_mail}" ]]
-    then
-	echo "INTERNAL ERROR: Searched for \"${mailto}\" and found \"${ldap_mail}\" in ldap.
-Strings \"${lowercase_mailto}\" and \"${lowercase_ldap_mail}\" dos not match" 1>&2
-	continue
-	# NOT REACHED
-    fi
+if $_unlocking_appoitment_found
+then
     
-    dn=$( sed -n -e '/^dn: /s/^dn: //p' <<< ${dn_search_result} )
 
-    appointed_DNs_array+=( "${dn}" )
+    for ics_url in $( echo "${ics_url_list}" )
+    do
 
-done
+	if [[ "${ics_url}" == "${_unlocking_appointment_icl_url}" ]]
+	then
+	    # skip this appointment since it is the one which is used to ulock reservation
+	    continue
+	fi
+
+	vcal_data=$( getVCalData ${ics_url} )
+
+	organizer_line=$(
+	    echo "${vcal_data}" | grep -e '^ORGANIZER;'
+		      )
+
+	organizer_data=$(
+	    echo "${organizer_line}" | sed -e 's/ORGANIZER;//' -e 's/\r$//'
+		      )
+
+	displayName=$( echo "${organizer_data}" | sed -e 's/CN=\(.*\):mailto:.*$/\1/' )
+	mailto=$( echo "${organizer_data}" | sed -e 's/.*:mailto:\(.*\)$/\1/' )
+
+	(
+	    echo "INFO: found appointment for display Name \"${displayName}\" with email \"${mailto}\""
+	) 1>&2
+
+	# retrieve user description (dn + mail) in LDAP, based on his email address (mailto)
+	dn_search_result=$(
+	    ${ldapsearch_cmd} -z 1 "mail=${mailto}" dn mail
+			)
+	if grep -q '--regexp=^mail:' <<< ${dn_search_result}
+	then
+	    # ldap search result OK
+	    :
+	else
+	    echo "INTERNAL ERROR: Could not file \"${mailto}\" in ldap" 1>&2
+	    continue
+	    # NOT REACHED
+	fi
+
+	#
+	# for security, we check that the retrieved mail is what we searched for
+	#
+	ldap_mail=$( sed -n -e '/^mail: /s/^mail: //p' <<< ${dn_search_result} )
+	lowercase_mailto=$( tr '[:upper:]' '[:lower:]' <<< ${mailto} )
+	lowercase_ldap_mail=$( tr '[:upper:]' '[:lower:]' <<< ${ldap_mail} )
+	if [[ "${lowercase_mailto}" != "${lowercase_ldap_mail}" ]]
+	then
+	    echo "INTERNAL ERROR: Searched for \"${mailto}\" and found \"${ldap_mail}\" in ldap.
+Strings \"${lowercase_mailto}\" and \"${lowercase_ldap_mail}\" dos not match" 1>&2
+	    continue
+	    # NOT REACHED
+	fi
+	
+	dn=$( sed -n -e '/^dn: /s/^dn: //p' <<< ${dn_search_result} )
+
+	appointed_DNs_array+=( "${dn}" )
+
+    done
+fi # if _unlocking_appoitment_found
 
 (
     echo "INFO: currently appointed DNs"
